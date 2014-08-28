e997fad970f25d237c4fbad15b890cd3a5da5632
Hassan <hassan.belhabib@seamless.se>
2014-08-28 11:21:43 +0200

    Updated app delegate and linked to source files

--- ProductManager/PMAppDelegate.m
+++ ProductManager/PMAppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "PMAppDelegate.h"
+#import "ProductsManagerViewController.h"
 
 @implementation PMAppDelegate
 
@@ -16,6 +17,7 @@
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
+    self.window.rootViewController = [[ProductsManagerViewController alloc] init];
     return YES;
 }
 
